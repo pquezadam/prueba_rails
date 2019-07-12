61649c7fdb1173edf84149b1fa766a207c29ddcc
pquezada <pquezada2009@hotmail.es>
2019-07-11 21:53:40 -0400

    todo_app

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  get 'todos', to: 'todos#index'
+  get 'todos/list'
+  get 'todos/new'
+  post 'todos', to:'todos#create'
+  get 'todos/:id', to: 'todos#show', as: 'todo'
+  get 'todos/:id/edit', to: 'todos#edit', as: 'todo_edit'
+  patch 'todos/:id', to: 'todos#update'
+  delete 'todos/:id', to: 'todos#destroy'
+  get 'todos/:id/complete', to: 'todos#complete', as: 'todo_complete'
+
+  root 'todos#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
